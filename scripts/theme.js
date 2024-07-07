2c5929d4b289be87b19bd50d092958b8a13f9af3
BiaPatatas <82343869+BiaPatatas@users.noreply.github.com>
2024-07-08 00:46:13 +0100

    bg

--- scripts/theme.js
+++ scripts/theme.js
@@ -1,11 +1,4 @@
-function setTheme(theme) {
-    const body = document.body;
-    const navbar = document.querySelector('.navbar');
-    if (theme === 'dark') {
-        body.classList.add('dark-mode');
-        navbar.classList.add('dark-mode');
-    } else {
-        body.classList.remove('dark-mode');
-        navbar.classList.remove('dark-mode');
-    }
-}
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
