59ec21ea9b5682a6ac7f26d4bf7b7d000f98eb5b
ricardo <ricardomcs2001@gmail.com>
2024-07-07 23:10:03 +0100

    fix - not finished

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const port = 3000;
 // Serve arquivos estáticos do diretório atual
 app.use(express.static('.'));
 
-
 const filePath = path.join(__dirname, 'screens', 'home.html');
 
 // Rota padrão para servir o home.html
